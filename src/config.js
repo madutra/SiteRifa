3b89f40f5595dde58657f74081871045cfbe84f1
caio <caiogaldino11@icloud.com>
2021-07-30 12:23:49 -0300

    13

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const config = {
     quantidade_max: 400,
     whatsapp_url: "https://wa.me/556282843200",
     whatsapp_frase: "Oi, desejo participar da rifa do Caio! Escolhi o número {numero_vem_aqui}",
-    preenchidos: []
+    preenchidos: [13,4,7,18,26,21,193,10,20,25,100,200,400,23,12,43,88,27,6,9,15,16,62,33,1,5,23,29,11,92,2,75,45,3,153,24,202,220,351,379,296,197,123,399]
 }
